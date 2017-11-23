0f86e85f9b0710a3a10e8ae31f23a5a3dd05bb85
bzabza <akkdlyapereliva@mail.ru>
2017-11-23 15:40:45 +0200

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,31 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 gulp.task('sass', function () {
-    return gulp.src('app/modules/**/**.sass')
+    return gulp.src('app/sass-styles/**/**.sass')
         .pipe(sass().on('error', sass.logError))
-        .pipe(concat('main.css'))
+        .pipe(concat('index-style.css'))
+        .pipe(gulp.dest('app/style/index-styles'))
+});
+
+
+gulp.task('sassNav', function () {
+    return gulp.src('app/modules/navigation/**.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('header.css'))
+        .pipe(gulp.dest('app/style'))
+});
+
+gulp.task('sassFoo', function () {
+    return gulp.src('app/modules/footer/**.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('footer.css'))
         .pipe(gulp.dest('app/style'))
 });
 gulp.task('watch', function () {
-    watch('app/modules/**/**.sass', function (a, b) {
-        gulp.run('sass');
-    });
+    watch('app/**/**/**.sass',
+        function (a, b) {
+            gulp.run('sassFoo','sassNav','sass');
+        });
 });
+
+
